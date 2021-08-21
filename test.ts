9db5870e40df75e2a0d84ddabc38f9b7749e6af0
kzhovn <kzhovn@gmail.com>
2021-08-21 17:12:15 -0400

    Plugin test

---
+++ test.ts
@@ -0,0 +1,17 @@
+import {Timer} from 'src/timer';
+import {DEFAULT_SETTINGS} from 'src/settings';
+import PomoTimerPlugin from 'src/main';
+import * as jest from 'jest';
+
+//no idea how to mock classes, let's leave this for now
+describe ('Timer', () => {
+    let timer: Timer;
+    let plugin: PomoTimerPlugin;
+    // jest.mock('src/main');
+
+
+    // beforeEach(() => {
+    //     timer = new Timer();
+    // })
+})
+
